98b8e3457283d06a25e38acdf4c39f04e77f3a0b
kevel choumene <k.choumene@cloudfacile.com>
2026-07-14 18:18:05 +0100

    feat: first commit for ledger management ref37445

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='openimis-be-ledger',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU AGPL v3',
+    description='The openIMIS Backend Ledger reference module.',
+    # long_description=README,
+    url='https://openimis.org/',
+    author='Kevel CHOUMENE',
+    author_email='keveltiah@gmail.com',
+    install_requires=[
+        'django',
+        'django-hordak',
+        'djangorestframework',
+        'openimis-be-claim',
+        'openimis-be-invoice',
+        'openimis-be-core',
+        'graphene-django',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
